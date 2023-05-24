b3e8cfaa73b1090b480070d5a40a751aac8d1523
kns98 <kns98@yahoo.com>
2023-05-24 09:13:05 +0800

    more tests

--- Spreadsheet/TestParser.cs
+++ Spreadsheet/TestParser.cs
@@ -7,11 +7,19 @@ namespace MicroSpread
     {
         public static void Test_Parser(string[] args)
         {
+            Test_Expr();
+            TestExpressionWithParentheses();
+            TestExpressionWithMismatchedParentheses();
+        }
+
+
+        public static void Test_Expr()
+        {
             // Create a sample expression
             string expression = "2 * (A1 + B2) - 5";
 
             // Create a SpreadsheetMirror object
-            var spreadsheetMirror = new SpreadsheetMirror(3,3);
+            var spreadsheetMirror = new SpreadsheetMirror(3, 3);
 
             // Set cell values
             spreadsheetMirror.SetCell(0, 0, 10);  // A1 = 10
@@ -24,9 +32,15 @@ namespace MicroSpread
             {
                 // Parse and evaluate the expression using the SpreadsheetMirror
                 double result = parser.Parse(spreadsheetMirror);
-                Debug.Assert(result == 55.0, "Result does not match the expected value.");
-                // Print the result
-                Console.WriteLine("Result: " + result);
+                int expectedValue = 55;
+
+                // Verify the result using assertions
+                if (result != expectedValue)
+                {
+                    Console.WriteLine("Result does not match the expected value of " + expectedValue);
+                }
+
+                Console.WriteLine("Parser Success - Result: " + result);
             }
             catch (Exception ex)
             {
@@ -34,7 +48,79 @@ namespace MicroSpread
                 Console.WriteLine("An error occurred: " + ex.Message);
             }
         }
+
+
+        public static void TestExpressionWithParentheses()
+        {
+            // Create a sample expression with parentheses
+            string expression = "(2 + 3) * (A1 + B2) - 5";
+
+            // Create a SpreadsheetMirror object
+            var spreadsheetMirror = new SpreadsheetMirror(3, 3);
+
+            // Set cell values
+            spreadsheetMirror.SetCell(0, 0, 10);  // A1 = 10
+            spreadsheetMirror.SetCell(1, 1, 20);  // B2 = 20
+
+            // Create an instance of the Parser class
+            var parser = new Parser(expression);
+
+            try
+            {
+                // Parse and evaluate the expression using the SpreadsheetMirror
+                double result = parser.Parse(spreadsheetMirror);
+                int expectedValue = 145;
+
+                // Verify the result using assertions
+                if (result != expectedValue)
+                {
+                    Console.WriteLine("Result does not match the expected value of " + expectedValue);
+                }
+
+                // Print the result
+                Console.WriteLine("Parser Success - Result: " + result);
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occur during parsing or evaluation
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+
+        public static void TestExpressionWithMismatchedParentheses()
+        {
+            // Create a sample expression with mismatched parentheses
+            string expressionWithMismatch = "((2 + 3) * (A1 + B2) - 5";
+
+            // Create a SpreadsheetMirror object
+            var spreadsheetMirror = new SpreadsheetMirror(3, 3);
+
+            // Set cell values
+            spreadsheetMirror.SetCell(0, 0, 10);  // A1 = 10
+            spreadsheetMirror.SetCell(1, 1, 20);  // B2 = 20
+
+            // Create an instance of the Parser class
+            var parserWithMismatch = new Parser(expressionWithMismatch);
+
+            try
+            {
+                // Parse and evaluate the expression with mismatched parentheses using the SpreadsheetMirror
+                double resultWithMismatch = parserWithMismatch.Parse(spreadsheetMirror);
+            }
+            catch (InvalidOperationException ex)
+            {
+                // Handle the exception for mismatched parentheses
+                Console.WriteLine("Invalid Parenthesis Exception throw as expected");
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception for mismatched parentheses
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
     }
 
-   
 }
+
+
+
